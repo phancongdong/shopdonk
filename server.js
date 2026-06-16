57b8b9d22741f1a602cfa752d74ad90b950d7225
phancongdong <phancongdong@users.noreply.github.com>
2026-06-16 13:16:52 +0700

    fix: Increase body parser limit to 50mb for large accounts list

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ const migrationRoutes = require('./routes/migration');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
